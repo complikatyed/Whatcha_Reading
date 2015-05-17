22d867b88115c930c6e8baa110771b3516c55af4
Katye Russell <katye.russell@gmail.com>
2015-05-17 16:24:25 -0500

    Added Database.execute structures.

---
+++ lib/database.rb
@@ -0,0 +1,42 @@
+require 'sqlite3'
+
+class Database
+
+  def self.load_structure
+    Database.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS books (
+      id integer PRIMARY KEY AUTOINCREMENT,
+      title varchar(200) NOT NULL,
+      start_date date,
+      end_date date,
+      topic varchar(200)
+      ranking integer
+    );
+    SQL
+    Database.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS authors (
+      id integer PRIMARY KEY AUTOINCREMENT,
+      last_name varchar (30) NOT NULL,
+      first_name varchar (30) NOT NULL
+    );
+    SQL
+    Database.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS genres (
+      id integer PRIMARY KEY AUTOINCREMENT,
+      name varchar (30) NOT NULL
+    );
+    SQL
+  end
+
+  def self.execute(*args)
+    initialize_database unless defined?(@@db)
+    @@db.execute(*args)
+  end
+
+  def self.initialize_database
+    environment = ENV["TEST"] ? "test" : "production"
+    database = "db/whatcha_reading_#{environment}.sqlite"
+    @@db = SQLite3::Database.new(database)
+    @@db.results_as_hash = true
+  end
+end
