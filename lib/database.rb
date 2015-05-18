6b9b740ca9632148e25dc9897717fe7da3c8db1c
Katye Russell <katye.russell@gmail.com>
2015-05-18 11:46:01 -0500

    Set up books database structure

--- lib/database.rb
+++ lib/database.rb
@@ -9,23 +9,31 @@ class Database
       title varchar(200) NOT NULL,
       start_date date,
       end_date date,
-      topic varchar(200)
-      ranking integer
+      topic varchar(200),
+      ranking integer,
+      genre_id integer NOT NULL
     );
     SQL
-    Database.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS authors (
-      id integer PRIMARY KEY AUTOINCREMENT,
-      last_name varchar (30) NOT NULL,
-      first_name varchar (30) NOT NULL
-    );
-    SQL
-    Database.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS genres (
-      id integer PRIMARY KEY AUTOINCREMENT,
-      name varchar (30) NOT NULL
-    );
-    SQL
+    # Database.execute <<-SQL
+    # CREATE TABLE IF NOT EXISTS authors (
+    #   id integer PRIMARY KEY AUTOINCREMENT,
+    #   last_name varchar (30) NOT NULL,
+    #   first_name varchar (30) NOT NULL
+    # );
+    # SQL
+    # Database.execute <<-SQL
+    # CREATE TABLE IF NOT EXISTS genres (
+    #   id integer PRIMARY KEY AUTOINCREMENT,
+    #   name varchar (30) NOT NULL
+    # );
+    # SQL
+    # Databse.execute <<-SQL
+    # CREATE TABLE IF NOT EXISTS book_authors (
+    #   id integer PRIMARY KEY AUTOINCREMENT,
+    #   book_id integer,
+    #   author_id integer
+    #   );
+    # SQL
   end
 
   def self.execute(*args)
