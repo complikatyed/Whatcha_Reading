1652959f917e6ab3a16cebd8f1f757cf4bb5f274
Katye Russell <katye.russell@gmail.com>
2015-05-18 11:47:32 -0500

    Added methods following in-class examples.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
-require "minitest/reporters"
-
+require 'minitest/reporters'
 Dir["./app/**/*.rb"].each { |f| require f }
 Dir["./lib/*.rb"].each { |f| require f }
 ENV["TEST"] = "true"
@@ -22,10 +21,10 @@ def create_book(title)
   Database.execute("INSERT INTO books (title) VALUES (?)", title)
 end
 
-def main_menu
-  "1. Add a book\n2. Edit book info\n3. View book list\n4. Exit\n"
-end
+# def main_menu
+#   "1. Add a book\n2. Edit book info\n3. View book list\n4. Exit\n"
+# end
 
-def actions_menu
-  "Would you like to?\n1. Edit\n2. Delete\n3. Exit\n"
-end
+# def actions_menu
+#   "Would you like to?\n1. Edit\n2. Delete\n3. Exit\n"
+# end
