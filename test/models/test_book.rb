3ab0f27229cb313eee62ef590fb01e31e8cbda1a
Katye Russell <katye.russell@gmail.com>
2015-05-18 13:04:58 -0500

    Added tests for 'book.valid?'  All are now passing.

--- test/models/test_book.rb
+++ test/models/test_book.rb
@@ -37,9 +37,59 @@ require_relative '../test_helper'
   			book.save
   			assert_equal 0, Book.count
   		end
-  		it "populates the error messages" do
+  		it "populates the error message" do
   			book.save
   			assert_equal "\"\" is not a valid book title.", book.errors
   		end
   	end
   end
+
+  describe ".valid?" do
+  	describe "with valid data" do
+  		let(:book){ Book.new("Mort") }
+  		it "returns true" do
+  			assert book.valid?
+  		end
+  		it "sets errors to nil" do
+  			book.valid?
+  			assert book.errors.nil?
+  		end
+  	end
+  	describe "with no title provided" do
+  		let(:book){ Book.new(nil) }
+  		it "returns false" do
+  			refute book.valid?
+  		end
+  		it "sets the error message" do
+  			book.valid?
+  			assert_equal "\"\" is not a valid book title.", book.errors
+  		end
+  	end
+  	describe "with empty string title" do
+  		let(:book){ Book.new("") }
+  		it "returns false" do
+  			refute book.valid?
+  		end
+  		it "sets the error message" do
+  			book.valid?
+  			assert_equal "\"\" is not a valid book title.", book.errors
+  		end
+  	end
+  	describe "with a previously invalid title" do
+  		let(:book){ Book.new("") }
+  		before do
+  			refute book.valid?
+  			book.title = "Carpe Jugulum"
+  			assert_equal "Carpe Jugulum", book.title
+  		end
+  		it "should return true" do
+  			assert book.valid?
+  		end
+  		it "should not return an error message" do
+  			book.valid?
+  			assert_nil book.errors
+  		end
+  	end
+  end
+
+
