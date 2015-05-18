15a04cb9760def91d8d323af127a8ffa72b937fa
Katye Russell <katye.russell@gmail.com>
2015-05-18 12:43:38 -0500

    Tests for '.save' valid and invalid models are now passing.

--- test/models/test_book.rb
+++ test/models/test_book.rb
@@ -6,3 +6,40 @@ require_relative '../test_helper'
       assert_equal "foo", book.title
     end
   end
+
+  describe ".save" do
+  	describe "if the model is valid" do
+  		let(:book){ Book.new("Guards, Guards") }
+  		it "should return true" do
+  			assert book.save
+  		end
+  		it "saves the model to the database" do
+  			book.save
+  			assert_equal 1, Book.count
+  			last_row = Database.execute("SELECT * FROM books")[0]
+  			database_title = last_row['title']
+  			assert_equal "Guards, Guards", database_title
+  		end
+  		it "populates the model with id from the database" do
+  			book.save
+  			last_row = Database.execute("SELECT * FROM books")[0]
+  			database_id = last_row['id']
+  			assert_equal database_id, book.id
+  		end
+  	end
+
+  	describe "if the model is invalid" do
+  		let(:book){ Book.new("") }
+  		it "returns false" do
+  			refute book.save
+  		end
+  		it "does not save the model to the database" do
+  			book.save
+  			assert_equal 0, Book.count
+  		end
+  		it "populates the error messages" do
+  			book.save
+  			assert_equal "\"\" is not a valid book title.", book.errors
+  		end
+  	end
+  end
