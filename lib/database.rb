7337c5da43ea67a2541813bc03dbdc8a50c1055c
Katye Russell <katye.russell@gmail.com>
2015-05-25 22:21:24 -0500

    Refactor to include active record.

--- lib/database.rb
+++ lib/database.rb
@@ -7,36 +7,12 @@ class Database
     CREATE TABLE IF NOT EXISTS books (
       id integer PRIMARY KEY AUTOINCREMENT,
       title varchar(200) NOT NULL,
-      genre_id, integer NOT NULL DEFAULT 0,
-      start_date date datetime DEFAULT (datetime('now','localtime')),
-      end_date date,
-      topic varchar(200) NOT NULL DEFAULT "not yet entered",
-      ranking integer NOT NULL DEFAULT 0,
-      FOREIGN KEY (genre_id) REFERENCES genres (id)
+      topic varchar(200),
+      rank integer,
+      start date,
+      finish date
     );
     SQL
-    Database.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS genres (
-      id integer PRIMARY KEY AUTOINCREMENT,
-      name varchar (30) NOT NULL
-    );
-    SQL
-    Database.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS authors (
-      id integer PRIMARY KEY AUTOINCREMENT,
-      last_name varchar (30) NOT NULL,
-      first_name varchar (30) NOT NULL
-    );
-    SQL
-    Database.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS book_authors (
-      id integer PRIMARY KEY AUTOINCREMENT,
-      book_id integer NOT NULL,
-      author_id integer NOT NULL,
-      FOREIGN KEY (book_id) REFERENCES books (id),
-      FOREIGN KEY (author_id) REFERENCES authors (id)
-     );
-     SQL
   end
 
   def self.execute(*args)
@@ -45,7 +21,7 @@ class Database
   end
 
   def self.initialize_database
-    environment = ENV["TEST"] ? "test" : "production"
+    environment = Environment.current
     database = "db/whatcha_reading_#{environment}.sqlite"
     @@db = SQLite3::Database.new(database)
     @@db.results_as_hash = true
